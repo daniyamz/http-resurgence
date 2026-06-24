011e856f9b82d7353efdccfd829c9446aea73efb
daniyamz <terkimbiterungwa@gmail.com>
2026-06-24 11:21:46 +0100

    Exe1

--- exer1.go
+++ exer1.go
@@ -13,5 +13,9 @@ func main() {
 	fmt.Println("Server is runing on port :8080, Press ctrl + c to ternimate the server")
 	mux := http.NewServeMux()
 	mux.HandleFunc("/method-inspector", inspectoHandler)
+	mux.HandleFunc("/echo", EchoHandler)
+	mux.HandleFunc("/headers", headersHandler)
+	mux.HandleFunc("/form", FormHandler)
+	mux.HandleFunc("/status", StatusCodeHandler)
 	http.ListenAndServe(":8080", mux)
 }
